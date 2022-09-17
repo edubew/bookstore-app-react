81bdfff34bab9905b6808db8a8aa79aeb14f3930
edubew <edubew98@gmail.com>
2022-09-17 13:45:24 +0300

    Add a form to the InputBook component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputBook from './components/Books/InputBook';
+
+const App = () => {
+  return (
+    <div>
+      <InputBook />
+    </div>
+  );
+}
+
+export default App;
