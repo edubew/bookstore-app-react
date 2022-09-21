1051027951a116b858662fc8ae63e00957ddf466
edubew <edubew98@gmail.com>
2022-09-21 17:30:05 +0300

    Add functionality to "Add New" button to dispatch the actions

--- src/components/Books/InputBook.js
+++ src/components/Books/InputBook.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './books.css';
 
-const InputBook = () => (
-  <div className="input__container">
-    <h1>ADD NEW BOOK</h1>
-    <form>
-      <input type="text" name="name" placeholder="Book title..." required />
-      <input type="text" name="name" placeholder="Author..." required />
-      <button className="add__btn" type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+const InputBook = () => {
+  const [state, setState] = useState({
+    title: '',
+    author: '',
+  });
+
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    dispatch(InputBook(state.title, state.author));
+    state.title = '';
+    state.author = '';
+  };
+
+    <div className="input__container">
+      <h1>ADD NEW BOOK</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Book title..." required onChange={handleChange} />
+        <input type="text" name="name" placeholder="Author..." required onChange={handleChange} />
+        <button className="add__btn" type="submit" onClick={handleSubmit}>ADD BOOK</button>
+      </form>
+    </div>;
+};
 
 export default InputBook;
