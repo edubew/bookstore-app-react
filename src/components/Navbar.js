bc3612afc1190276a2a19370e345b1bc4c13a645
edubew <edubew98@gmail.com>
2022-09-30 11:25:53 +0300

    Add styling to the bookstore app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GoPerson } from 'react-icons/go';
 import './Books/books.css';
 
 const Navbar = () => (
@@ -9,6 +10,7 @@ const Navbar = () => (
     <nav>
       <Link className="nav__item" to="/">BOOKS</Link>
       <Link className="nav__item" to="/categories">CATEGORIES</Link>
+      <GoPerson className="login" />
     </nav>
   </header>
 );
